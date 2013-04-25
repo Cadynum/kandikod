9426522fb131b15d0f1b98f37aef72763f874d50
Christoffer Öjeling <christoffer@ojeling.net>
2013-04-25 14:52:00 +0200

    auto

--- com/sender/sender.ino
+++ com/sender/sender.ino
@@ -4,8 +4,8 @@
 #define tty Serial
 #define bt Serial1
 
-const int cal_low = 3;
-const int cal_high = 2;
+const int cal_low = 5;
+const int cal_high = 3;
 
 const int pin_flex[] = {A0, A1, A2, A3, A4, A5};
 
@@ -16,10 +16,10 @@ Control ctl;
 
 
 void setup() {
-	tty.begin(115200);
-	bt.begin(115200);
 	pinMode(cal_low, INPUT);
 	pinMode(cal_high, INPUT);
+	tty.begin(115200);
+	bt.begin(115200);
 }
 
 void printcal() {
@@ -35,16 +35,22 @@ void printcal() {
 }
 
 void loop() {
-	if (digitalRead(cal_low)) {
+	int v1 = digitalRead(cal_low);
+	int v2 = digitalRead(cal_high);
+	if (!v1) {
 		for (unsigned int i=0; i<SENSORS; i++) {
 			ref_low[i] = analogRead(pin_flex[i]);
 		}
-	}
-	if (digitalRead(cal_high)) {
+		// tty.println("LOcal");
+	} else if (!v2) {
 		for (unsigned int i=0; i<SENSORS; i++) {
 			ref_high[i] = analogRead(pin_flex[i]);
 		}
+		// tty.println("HIcal");
 	}
+	// tty.print(v1);
+	// tty.print(' ');
+	// tty.println(v2);
 
 	for (unsigned int i=0; i<SENSORS; i++) {
 		int val = analogRead(pin_flex[i]);
