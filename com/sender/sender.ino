a014eaf179e27888e4ec69439b2a3813af508490
Christoffer Öjeling <christoffer@ojeling.net>
2013-05-06 16:30:51 +0200

    wee

--- com/sender/sender.ino
+++ com/sender/sender.ino
@@ -1,5 +1,6 @@
 #include "com.h"
 #include "state.h"
+#include "butterworth.h"
 
 #define tty Serial
 #define bt Serial1
@@ -9,11 +10,14 @@ const int cal_high = 3;
 
 const int pin_flex[] = {A0, A1, A2, A3, A4, A5};
 
-const int low_bound[] = {98, 115, 160, 90, 30, 30};
-const int high_bound[] = {50, 30, 50, 159, 120, 150};
+const int open_bound[] = {110, 85, 155, 150, 30, 35};
+const int closed_bound[] = {30, 27, 50, 45, 150, 150};
+// const int open_bound[] = {98, 115, 160, 90, 30, 30};
+// const int closed_bound[] = {50, 30, 50, 159, 120, 150};
 
-unsigned int ref_low[SENSORS];
-unsigned int ref_high[SENSORS];
+unsigned int ref_open[SENSORS];
+unsigned int ref_closed[SENSORS];
+struct bw_state filter_s[SENSORS];
 Control ctl;
 
 
@@ -29,25 +33,61 @@ void printcal() {
 	tty.print("CAL");
 	for (unsigned int i=0; i<SENSORS; i++) {
 		tty.print("(");
-		tty.print(ref_low[i]);
+		tty.print(ref_open[i]);
 		tty.print(", ");
-		tty.print(ref_high[i]);
+		tty.print(ref_closed[i]);
 		tty.print(")  ");
 	}
 	tty.print("\r\n");
 }
 
+void benchmark() {
+	static unsigned long lasttime = 0;
+	static unsigned int counter = 0;
+
+	if (counter == 1000) {
+		unsigned long now = millis();
+		unsigned long diff = now - lasttime;
+		counter = 0;
+		lasttime = now;
+		tty.print("FPS: ");
+		tty.println((double)1000000/diff);
+	} else {
+		counter++;
+	}
+}
+
+/* Assumes frames are always faster than 10000us. (which they are)
+ * If they are not it will stop to work ~70min after power on.
+ */
+const unsigned long us_per_frame = 10000UL;
 void loop() {
+	unsigned long t_start = micros();
+	unsigned long expect = t_start;
+
+	for (;;) {
+		expect += us_per_frame;
+		loop_real();
+		unsigned long now = millis();
+		if (now < expect) {
+		 	delayMicroseconds(expect - now);
+		}
+	}
+}
+
+bw_state apa = {0, 0};
+void loop_real() {
+	benchmark();
 	int v1 = digitalRead(cal_low);
 	int v2 = digitalRead(cal_high);
 	if (!v1) {
 		for (unsigned int i=0; i<SENSORS; i++) {
-			ref_low[i] = analogRead(pin_flex[i]);
+			ref_open[i] = analogRead(pin_flex[i]);
 		}
 		// tty.println("LOcal");
 	} else if (!v2) {
 		for (unsigned int i=0; i<SENSORS; i++) {
-			ref_high[i] = analogRead(pin_flex[i]);
+			ref_closed[i] = analogRead(pin_flex[i]);
 		}
 		// tty.println("HIcal");
 	}
@@ -56,18 +96,23 @@ void loop() {
 	// tty.println(v2);
 
 	for (unsigned int i=0; i<SENSORS; i++) {
+		// int val = butterworth(filter_s+i, analogRead(pin_flex[i]));
 		int val = analogRead(pin_flex[i]);
-		val = constrain(val, ref_low[i], ref_high[i]);
+		val = constrain(val, ref_open[i], ref_closed[i]);
 		ctl.flex[i] = map(val
-			, ref_low[i], ref_high[i]
-			, low_bound[i], high_bound[i]);
+			, ref_open[i], ref_closed[i]
+			, open_bound[i], closed_bound[i]);
 	}
 
 
 	send_bytes(&bt, (byte*)&ctl, sizeof(ctl));
 	printcal();
-	delay(10);
+	// unsigned int val = 1023 - (unsigned int)analogRead(pin_flex[1]);
+	// unsigned int fval = butterworth(&apa, val);
+	// tty.print(val);
+	// tty.print(" ");
+	// tty.println(fval);
+	// delay(10);
 	// // tty.println("Skickat ");
 	// ctl.print(&tty);
-	// delay(10);
 }
